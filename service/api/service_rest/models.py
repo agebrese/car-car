9cd6fce6007c05f5b2bf8148910fab9e4f22936d
Zachary <schmidtzachary16@gmail.com>
2023-09-05 18:38:36 -0700

    Finished poller and Models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=100)
+    sold = models.BooleanField()
+
+    def __str__(self):
+        return self.vin
+
+
+class Appiontment(models.Model):
+    date_time = models.DateTimeField()
+    resaon = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    vin = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name='technician',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.customer

b3df30663ba07b31ad7ae23ebf8ef2391beb0ad3
Abraham <your@email.here>
2023-09-07 12:27:02 -0700

    Finished SalesHistory.js

--- ghi/app/src/SalesHistory.js
+++ ghi/app/src/SalesHistory.js
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react';
 function SalesHistory() {
     const [sale, setSale] = useState([])
     const [salesperson, setSalesperson] = useState([]);
+    const [selectSalesData, setselectSalesData] = useState({
+        salesperson: "",
+    })
 
     const getData = async () => {
         const response = await fetch('http://localhost:8090/api/sales/');
@@ -33,12 +36,25 @@ function SalesHistory() {
         getSalesPersonData();
     }, [])
 
+    const handleSalesPersonChange = (e) => {
+        const value = e.target.value;
+        const inputName = e.target.name;
+        setselectSalesData({
+            //Previous form data is spread (i.e. copied) into our new state object
+            ...selectSalesData,
+            //On top of the that data, we add the currently engaged input key and value
+            [inputName]: value
+        });
+
+    }
+
 
 
 
     return (
         <>
-            <select name="salesperson" className="form-select">
+            <h1 className='mt-5'>Salesperson History</h1>
+            <select onChange={handleSalesPersonChange} value={selectSalesData.salesperson} name="salesperson" id="salesperson" className="form-select mt-5">
                 <option value="">Choose an Salesperson</option>
                 {salesperson.map(salesperson => {
                     return (
@@ -59,14 +75,15 @@ function SalesHistory() {
                 </thead>
                 <tbody>
                     {sale.map(sale => {
-                        return (
-                            <tr key={sale.href}>
-                                <td className='text-center align-middle'>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
-                                <td className='text-center align-middle'>{sale.customer.first_name} {sale.customer.last_name}</td>
-                                <td className='text-center align-middle'>{sale.automobile.vin}</td>
-                                <td className='text-center align-middle'>{sale.price}</td>
-                            </tr>
-                        );
+                        if (selectSalesData.salesperson === sale.salesperson.employee_id)
+                            return (
+                                <tr key={sale.href}>
+                                    <td className='text-center align-middle'>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
+                                    <td className='text-center align-middle'>{sale.customer.first_name} {sale.customer.last_name}</td>
+                                    <td className='text-center align-middle'>{sale.automobile.vin}</td>
+                                    <td className='text-center align-middle'>{sale.price}</td>
+                                </tr>
+                            );
                     })}
                 </tbody>
             </table >

d9f35003a989f0e4d2cfce69f9f1d0f01b4060ef
Abraham <your@email.here>
2023-09-14 21:54:50 -0700

    Fixed error with displaying a saleperson sale history

--- ghi/app/src/SalesHistory.js
+++ ghi/app/src/SalesHistory.js
@@ -4,9 +4,7 @@ import { useEffect, useState } from 'react';
 function SalesHistory() {
     const [sale, setSale] = useState([])
     const [salesperson, setSalesperson] = useState([]);
-    const [selectSalesData, setselectSalesData] = useState({
-        salesperson: "",
-    })
+    const [selectSalesData, setselectSalesData] = useState('')
 
     const getData = async () => {
         const response = await fetch('http://localhost:8090/api/sales/');
@@ -38,17 +36,9 @@ function SalesHistory() {
 
     const handleSalesPersonChange = (e) => {
         const value = e.target.value;
-        const inputName = e.target.name;
-        setselectSalesData({
-            //Previous form data is spread (i.e. copied) into our new state object
-            ...selectSalesData,
-            //On top of the that data, we add the currently engaged input key and value
-            [inputName]: value
-        });
+        setselectSalesData(value);
 
     }
-
-
 
 
     return (
@@ -75,7 +65,7 @@ function SalesHistory() {
                 </thead>
                 <tbody>
                     {sale.map(sale => {
-                        if (selectSalesData.salesperson === sale.salesperson.employee_id)
+                        if (parseInt(selectSalesData) === sale.salesperson.employee_id) {
                             return (
                                 <tr key={sale.href}>
                                     <td className='text-center align-middle'>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
@@ -84,6 +74,7 @@ function SalesHistory() {
                                     <td className='text-center align-middle'>{sale.price}</td>
                                 </tr>
                             );
+                        }
                     })}
                 </tbody>
             </table >
